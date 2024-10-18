e836b095f8e944f8bf4b164412016544e8a69256
Azmat-7860 <azmatalli01234@gmail.com>
2024-10-18 16:13:37 +0530

    first innitialized

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FcDownload } from "react-icons/fc";
+import { MdOutlineRefresh } from "react-icons/md";
+import { MdElectricBolt } from "react-icons/md";
+import Loding from "./Component/Loding";
+const HuggingFaceImage = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const inputRef = useRef(null);
+  const API_TOKEN = import.meta.env.VITE_API_KEY;
+
+  const handleSearch = () => {
+    const myInput = inputRef.current.value;
+
+    setError(null);
+    query({ inputs: myInput });
+  };
+
+  // Fetching function
+  const query = async (data) => {
+    try {
+      setLoading(true);
+      setImageUrl(null);
+
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+        {
+          headers: {
+            Authorization: `Bearer ${API_TOKEN}`, // Replace with your actual token
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch image");
+      }
+      // Get image blob from response
+      // console.log("this is respoonse ", response);
+      const result = await response.blob();
+      // console.log("this is bolb ", result);
+      // Convert blob to image URL
+      const imageUrl = URL.createObjectURL(result);
+      setImageUrl(imageUrl);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false); // Stop loading once API call finishes
+    }
+  };
+
+  return (
+    <div className=" myContainer w-100 d-flex justify-content-center align-items-center">
+      <div className="containerBody border border-primary-subtle p-3 rounded-2 text-center">
+        <h1>Text-to-Image Model</h1>
+        <p>Enter a prompt to generate an image.</p>
+        {/* <label htmlFor="input">Enter a prompt to generate an image.</label> */}
+        <input
+          id="input"
+          type="text"
+          className=" myInput "
+          ref={inputRef}
+          placeholder="Type your prompt here"
+        />
+        <button className="btn btn-outline-primary my-3" onClick={handleSearch}>
+          {!error ? (
+            <>
+              Create <MdElectricBolt size={20} />
+            </>
+          ) : (
+            <>
+              Retry <MdOutlineRefresh size={20} />
+            </>
+          )}
+        </button>
+
+        {loading && <Loding />}
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        {!loading && imageUrl && (
+          <div className="imgBody">
+            <img
+              className="myImage"
+              src={imageUrl}
+              alt="Generated from Hugging Face"
+            />
+            <a
+              className="downloadBtn"
+              href={imageUrl}
+              download="generated-image.png"
+            >
+              <FcDownload size={30} />
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HuggingFaceImage;
---
+++ src/Component/Loding.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loding = () => {
+  return (
+    <div className=" w-100 d-flex justify-content-center">
+      <div class="spinner-border text-primary my-5" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loding;
